d474135ddddb003efbd38f8977c70051691acf59
Josh Hadro <josh.hadro@gmail.com>
2018-07-01 16:27:14 -0400

    update js

--- assets/js/tsne-webgl.js
+++ assets/js/tsne-webgl.js
@@ -791,12 +791,18 @@ function onMousemove(event) {
 }
 
 
+/** Adding function to open item in a new page **/
+
 function onDblclick(event) {
   var hash = window.location.href.split('/#')[1];
   if (hash) {
-  window.open('http://dp.la/item/'+hash);
+  window.open('https://dp.la/item/'+hash);
   }
 }
+
+
+
+
 
 /**
 * Store the previous mouse position so that when the next
@@ -941,3 +947,16 @@ var controls = getControls(camera, renderer);
 addCanvasEventListeners()
 addWindowEventListeners()
 loadData()
+
+
+/** TRYING TO MAKE SLIGHTLY MORE ACCESSIBLE **/
+
+// // controls
+//         controls = new THREE.OrbitControls( camera, renderer.domElement );
+//         //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+//         controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+//         controls.dampingFactor = 0.25;
+//         controls.screenSpacePanning = false;
+//         controls.minDistance = 100;
+//         controls.maxDistance = 500
+//         controls.maxPolarAngle = Math.PI / 2;
