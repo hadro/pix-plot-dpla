6dc51bbe4b03e0cd6ffd884861c285d0a0a43e66
Josh Hadro <josh.hadro@gmail.com>
2018-07-02 16:14:36 -0400

    cleanup

--- dpla_scrape.py
+++ dpla_scrape.py
@@ -9,15 +9,15 @@ images = [x for x in df.ix[:, 0]]
 base = 'https://dp.la/thumb/'
 
 for i in images: 
-	r = requests.get(base + str(i))
-	header = r.headers#
-	if r.status_code == 200:
-		if not os.path.isfile('./thumbs/'+i+'.jpg'):
+	if not os.path.isfile('./thumbs/'+i+'.jpg'):
+		r = requests.get(base + str(i))
+		if r.status_code == 200:
 			print("Downloading: ", i)
 			with open('./thumbs/'+i+'.jpg',"wb") as f:
 				f.write(r.content)
-		else: 
-			print('ALREADY DOWNLOADED: ', i)
-	else:
-		with open('error_log.txt', 'a') as f:
-			f.write("ERROR: " + i+'\n')
+		else:
+			with open('error_log.txt', 'a') as f:
+				f.write("ERROR: " + i+'\n')
+	else: 
+		print('ALREADY DOWNLADED ', i)
+	
